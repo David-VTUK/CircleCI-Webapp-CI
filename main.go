60d8607e6b04e741b49a21f497d703ec300d7482
DH-Rancher <david.holder@rancher.com>
2020-08-18 12:17:28 +0100

    Test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "CI test webserver 8")
+	fmt.Fprintf(w, "CI test webserver 9")
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -12,7 +12,7 @@ func TestHelloWorld(t *testing.T) {
 
 	helloWorld(response, request)
 	got := response.Body.String()
-	want := "CI test webserver 8"
+	want := "CI test webserver 9"
 
 	if got != want {
 		t.Error("Wanted:", want, "Got:", got)
