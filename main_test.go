ea52cd7e247eea329662777eeaf568d25c241dc7
DH-Rancher <david.holder@rancher.com>
2020-08-18 12:41:57 +0100

    More Testing

--- main_test.go
+++ main_test.go
@@ -12,7 +12,7 @@ func TestHelloWorld(t *testing.T) {
 
 	helloWorld(response, request)
 	got := response.Body.String()
-	want := "CI test webserver 9"
+	want := "CI test webserver 10"
 
 	if got != want {
 		t.Error("Wanted:", want, "Got:", got)
